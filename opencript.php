a33d0f2b22a44348c2bfb7d436b951fa0a1fc622
Pablo Verlly <pablov3rlly@gmail.com>
2017-09-10 22:56:14 -0300

    Update opencript.php

--- opencript.php
+++ opencript.php
@@ -1,17 +1,4 @@
 <?php
-/* OPENCART CUSTOMER PASSWORD HASH GENERATOR
-
-- USAGE
-		php opencript.php password salt
-
-- DEMO
-
-C:\Users\demo\opencript> php opencript.php admin oInuc412L
-RESULT: d69ab6a7b73bf52fe58adf3ba6b9c84ee27a0ad0
-
-*/
-
-
 $password = $argv[1];
 $salt = $argv[2];
 
