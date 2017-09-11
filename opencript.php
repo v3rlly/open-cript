c9a413a873e6d968c471290f2de04763e4ea7494
Pablo Verlly <pablov3rlly@gmail.com>
2017-09-11 16:31:20 -0300

    Update opencript.php

--- opencript.php
+++ opencript.php
@@ -2,7 +2,7 @@
 #This is a simple tool for discovering passwords stored in application databases that use OpenCart.
 #Enjoy and be creative.
 
-ini_set('memory_limit', '1024M'); #configurable value
+ini_set('memory_limit', -1); #configurable value
 
 $presentation = "
 \n||||||||||||||   -   OPENCART PASSWORD DECRYPTOR BASED ON WORD LIST   -   ||||||||||||||\n
@@ -22,34 +22,32 @@ Git: github.com/pabloverlly
 if( empty($argv[1]) OR  empty($argv[2]) OR empty($argv[3]) ){	echo $presentation;	}
 
 else {
-$size = 0;
-$pwd = $argv[1];
-$salt = $argv[2];
-$wl = $argv[3];
-$lin = file($wl, FILE_IGNORE_NEW_LINES);
-$in = microtime(true);
+	$size = 0;
+	$pwd = $argv[1];
+	$salt = $argv[2];
+	$wl = $argv[3];
+	$lin = file($wl, FILE_IGNORE_NEW_LINES);
+	$in = microtime(true);
 
-echo "\nPASSWORD: ".$pwd."\nSALT: ".$pwd."\n";
+	echo "\nPASSWORD: ".$pwd."\nSALT: ".$salt."\n";
 
 
 function out($str,$found,$in,$size){
-$t = microtime(true) - $in;
+	$t = microtime(true) - $in;
 	
 	if($found==1) {
-		echo "\n\n Found: ".$str."\n TIME:".$t."\n"."\n NUMBER OF STRINGS TESTED:".$size."\n";
+		echo "\n\n Found: ".$str."\n TIME:".$t."\n"."\n NUMBER OF WORDS TESTED:".$size."\n";
 	}
 	else {
-		echo "\n\n Not Foud\n TIME: ".$t."\n"."\n NUMBER OF STRINGS TESTED:".$size."\n";
+		echo "\n\n Not Foud\n TIME: ".$t."\n"."\n NUMBER OF WORDS TESTED:".$size."\n";
 	}
 }
 
 foreach($lin as $str)
-{
-   $hash=SHA1($salt.SHA1($salt.SHA1($str)));
-   $size+=1;
-   if($hash==$pwd) { out($str,1,$in,$size); break; } else{}
-   if(!next($lin)) { out($str,0,$in,$size); break; } else{}
-}
-
-
+	{
+	   $hash=SHA1($salt.SHA1($salt.SHA1($str)));
+	   $size+=1;
+	   if($hash==$pwd) { out($str,1,$in,$size); break; } else{}
+	   if(!next($lin)) { out($str,0,$in,$size); break; } else{}
+	}
 }?>
