5b7ae4868f52233387444a0bb8b59ecb287a4947
Pablo Verlly <pablov3rlly@gmail.com>
2017-09-11 11:03:03 -0300

    Update opencript.php

--- opencript.php
+++ opencript.php
@@ -4,17 +4,17 @@ $salt = $argv[2];
 $wordlist = $argv[3];
 
 $lines = file($wordlist, FILE_IGNORE_NEW_LINES);
-$start = microtime(true);
+$time0 = microtime(true);
 
 foreach($lines as $string)
 {
    $hashed=SHA1($salt.SHA1($salt.SHA1($string)));
-   if($hashed==$password){tempo($string,$password,'1',$start,$salt); break;}else{}
-   if(!next($lines) ) { tempo($string,$password,'0',$start,$salt); break;} else{}
+   if($hashed==$password){out($string,$password,'1',$time0,$salt); break;}else{}
+   if(!next($lines) ) { out($string,$password,'0',$time0,$salt); break;} else{}
 }
 
-function tempo($string,$hashed,$found,$start,$salt){
-$time = microtime(true) - $start;
+function out($string,$hashed,$found,$start,$salt){
+$time = microtime(true) - $time0;
 
 	if($found=='1') {
 		echo "\nHASH:".$hashed."\nSALT:".$salt."\nPASSWORD:".$string."\nTIME:".$time."\n";
