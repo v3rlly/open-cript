0b2d3d6dc986b9eccc219f89eb058f9855e4021a
Pablo Verlly <pablov3rlly@gmail.com>
2017-09-12 03:05:32 -0300

    versão atualizada para testar maiores listas.
    Version updated with support for large word lists.
    try it: https://crackstation.net/buy-crackstation-wordlist-password-cracking-dictionary.htm

--- opencript.php
+++ opencript.php
@@ -1,11 +1,13 @@
 <?php
-#This is a simple tool for discovering passwords stored in application databases that use OpenCart.
+#This is a tool for discovering passwords stored in application databases that use OpenCart.
 #Enjoy and be creative.
+#Updated version, Test giant word lists.
 
-ini_set('memory_limit', -1); #configurable value
+set_time_limit(0);
+ini_set('memory_limit', '2048M'); #configurable value
 
-$presentation = "
-\n||||||||||||||   -   OPENCART PASSWORD DECRYPTOR BASED ON WORD LIST   -   ||||||||||||||\n
+$press = "
+\n   OPENCART PASSWORD DECRYPTOR BASED ON WORD LIST   \n\n
 USAGE:
 	php opencript.php <HASH> <SALT> <WORDLIST>
 
@@ -14,40 +16,131 @@ EXEMPLE:
 	php opencript.php f2e9efd4a366507c5b1cba7749659d93d61ae335 oInuc412L wordlist_demo.txt\n
 
 
-Author: Pablo Verlly
-Contact: pablov3rlly@gmail.com
-Git: github.com/pabloverlly
+-------------------------------------
+| Author: Pablo Verlly
+| Contact: pablov3rlly@gmail.com
+| Git: github.com/pabloverlly
+-------------------------------------
 ";
 
-if( empty($argv[1]) OR  empty($argv[2]) OR empty($argv[3]) ){	echo $presentation;	}
+if( empty($argv[1]) OR  empty($argv[2]) OR empty($argv[3]) )		{	echo $press;	}
 
 else {
-	$size = 0;
+	$size=0;
 	$pwd = $argv[1];
 	$salt = $argv[2];
 	$wl = $argv[3];
-	$lin = file($wl, FILE_IGNORE_NEW_LINES);
 	$in = microtime(true);
 
 	echo "\nHASH: ".$pwd."\nSALT: ".$salt."\n";
 
 
-function out($str,$found,$in,$size){
+
+#<<---- Recebe o nome da parte da wordlist e testa ---->>#
+function arq($arqv,$size_array)	{
+		global $size;
+		global $salt;
+		global $pwd;
+		global $in;
+
+		$arqv = array_values($arqv);
+
+
+		echo "\nTest started: ";
+		echo "\nNumber of wordlists: " . sizeof($arqv)."\n\n";
+
+		foreach ($arqv as $key) {
+
+			$lin = file($key, FILE_IGNORE_NEW_LINES);
+			$position=array_search($key, $arqv);
+
+			echo "\n Testing Wordlist ".$position." ..";
+
+
+		#<<---- Recebe o nome da parte da wordlist e testa ---->>#
+		foreach ($lin as $str) {
+
+			$hash=SHA1($salt.SHA1($salt.SHA1($str)));
+			$size+=1;
+			$t = microtime(true) - $in;
+			if($hash==$pwd) {		echo "\n\n\n 		HASH FOUND: ".$str."\n 		TIME: ".$t."\n 		NUMBER OF STRINGS TESTED: ".$size."\n"; exit;		}
+		}
+
+			echo "\nNOT FOUND IN THIS WORDLIST PART: \n";
+			unset($lin);
+	}
+
 	$t = microtime(true) - $in;
-	
-	if($found==1) {
-		echo "\n\n Found: ".$str."\n TIME:".$t."\n"."\n NUMBER OF STRINGS TESTED:".$size."\n";
-	}
-	else {
-		echo "\n\n Not Foud\n TIME: ".$t."\n"."\n NUMBER OF STRINGS TESTED:".$size."\n";
-	}
+	echo "\n\nHASH NOT FOUND IN THIS WORDLIST";
 }
 
-foreach($lin as $str)
-	{
-	   $hash=SHA1($salt.SHA1($salt.SHA1($str)));
-	   $size+=1;
-	   if($hash==$pwd) { out($str,1,$in,$size); break; } else{}
-	   if(!next($lin)) { out($str,0,$in,$size); break; } else{}
-	}
+
+
+#<<---- Checa se o arquivo é maior que 200MB E DIVIDE EM PARTES MENORES ---->>#
+if (filesize($wl)>=200000000) {
+
+	$arqv = array();
+	$i=0;
+
+	#<<---- AÇÕES PARA WINDOWS ---->>#
+	if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+		{
+			exec("CACLS * /e /p %USERNAME%:f");
+			exec("split $wl -C100m part_");
+
+			$dir = '.\\';
+			$files = scandir($dir);
+
+
+			foreach ($files as $name)
+			{
+				if (substr($name, 0, 3)=='par') {	$arqv[$i] = $name;	}
+				$i++;
+			}
+
+			$size_array = count($arqv);
+			arq($arqv,$size_array);
+		}
+
+
+
+	#<<---- AÇÕES PARA LINUX ---->>#
+	elseif(strtoupper(substr(PHP_OS, 0, 3)) === 'LIN')
+		{
+			exec("chmod 775 *");
+			exec("split $wl -C100m part_");
+
+			$dir = './';
+			$files = scandir($dir);
+
+			foreach ($files as $name)
+			{
+				if (substr($name, 0, 3)=='par') {	$arqv[$i] += $name;	}
+				$i++;
+			}
+			
+			$size_array = count($arqv);
+			arq($arqv,$size_array);
+		}
+
+
+
+	#<<---- AÇÕES PARA OUTROS SISTEMAS ---->>#
+	else{	echo '\n System not supported. Try using a word list smaller than 200mb ';	}
+
+
+}
+
+	#<<---- CASO A WORDLIST SEJA MENOR QUE 200mb ---->>#
+
+	$lin = file($wl, FILE_IGNORE_NEW_LINES);
+	foreach($lin as $str)
+		{
+			$hash=SHA1($salt.SHA1($salt.SHA1($str)));
+			$size+=1;
+			$t = microtime(true) - $in;
+			if($hash==$pwd) {		echo "\n\n\n 		HASH FOUND: ".$str."\n 		TIME: ".$t."\n 		NUMBER OF STRINGS TESTED: ".$size."\n"; exit;		}
+			if(!next($lin)) { 		echo "\n\nHASH NOT FOUND IN THIS WORDLIST"; exit;		}
+		}
+
 }?>
